f7b78510b35ee604615717fd566204f7796567a0
Ashwini Yadrami <yadramiash192380@gmail.com>
2024-10-17 11:21:40 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace OverflowExceptionPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                checked
+                {
+                    int a = int.MaxValue;
+                    Console.WriteLine(a + 1);
+                }
+            }
+            catch (OverflowException ov) 
+            {
+                Console.WriteLine("its out of range");
+            }
+        }
+    }
+}
